d52626d42231498f432b9d7ed15f4456d04aac2e
Denis Kazantsev <denspa@mail.ru>
2022-01-27 00:30:45 +0600

    поправил градл

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.kazantsev.buildsrc.Libs.Navigation.nav_version
+
 
 buildscript {
     repositories {
@@ -11,7 +11,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0-rc01")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${com.kazantsev.buildsrc.Libs.Navigation.nav_version}")
     }
 }
 
